b731c6b29d886fcca899d736712f453a498a472f
Jon Simington <jon.simington@mst.edu>
2016-02-03 19:22:43 -0600

    Add base.html and allauth templates

--- cs4096/cs4096/urls.py
+++ cs4096/cs4096/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth.views import logout_then_login
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('lists.urls')),
+    url(r'^$', include('lists.urls')),
 
     # Django AllAuth
-    url(r'^accounts/logout/$', logout_then_login),
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/logout/$', logout_then_login),
 ]
