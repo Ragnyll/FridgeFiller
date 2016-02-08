5d38dadc2bb1d3f822c7d2681958e35e2e778bb1
Jon Simington <jon.simington@mst.edu>
2016-02-08 13:35:44 -0600

    Add base pages for the lists app

--- fridgefiller/lists/urls.py
+++ fridgefiller/lists/urls.py
@@ -1,8 +1,11 @@
 # urls for the list application
 
 from django.conf.urls import url
-from views import *
+from views import ListsView, ListView, NewListView, EditListView
 
 urlpatterns = [
- 
+    url(r'^$', ListsView.as_view(), name="lists"),
+    url(r'^(?P<list_id>\d+)', ListView.as_view()),
+    url(r'^new', NewListView.as_view(), name="new-list"),
+    url(r'^edit/(?P<list_id>\d+)', EditListView.as_view()),
 ]
