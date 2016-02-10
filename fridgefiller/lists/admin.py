44f5c2ebd90a60fc1acd660c9fc52c679aa9a737
pmbb83 <pmbb83@mst.edu>
2016-02-10 11:26:07 -0600

    Specified list_display variable for each model in out list app

--- fridgefiller/lists/admin.py
+++ fridgefiller/lists/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(UserProfile)
-admin.site.register(Item)
-admin.site.register(Party)
-admin.site.register(ShoppingList)
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description')
+
+class GroupAdmin(admin.ModelAdmin):
+	list_display = ('name', 'owner')
+
+class ListAdmin(admin.ModelAdmin):
+	list_display = ('name', 'primary_owner', 'description')
+
+class ItemAdmin(admin.ModelAdmin):
+	list_display = ('name', 'cost', 'description')
+
+class IngredientAdmin(admin.ModelAdmin):
+	list_display = ('name', 'recipe', 'unit', 'amount')
+
+class RecipeAdmin(admin.ModelAdmin):
+	list_display = ('recipe_name', 'serving_size')
+
+class NutritionAdmin(admin.ModelAdmin):
+	list_display = ('calories', 'total_fat', 'sodium', 'sugar', 'protein')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(List, ListAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Nutrition, NutritionAdmin)
