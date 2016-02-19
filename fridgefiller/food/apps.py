5446faa2f1f3220435929f6ab936147332fe3fe6
brandon <bwp44f@mst.edu>
2016-02-18 18:41:59 -0600

    deleted food app

--- fridgefiller/food/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class FoodConfig(AppConfig):
-    name = 'food'
--- fridgefiller/food/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from views import RecipesView, NewRecipeView
-
-urlpatterns = [
-    url(r'^$', RecipesView.as_view(), name="recipes"),
-    url(r'^new-recipe', NewRecipeView.as_view(), name="new_recipe"),
-]
