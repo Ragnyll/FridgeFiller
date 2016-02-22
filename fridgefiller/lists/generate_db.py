ddffa3bb97daa393b3281d526ffbba0e2e34cc5a
brandon <bwp44f@mst.edu>
2016-02-21 20:12:22 -0600

    added json data fixed script to generate an empty db

--- fridgefiller/lists/generate_db.py
+++ fridgefiller/lists/generate_db.py
@@ -2,24 +2,18 @@
 import sys, os
 file_path = __file__
 
-app_loc = os.path.dirname(os.path.abspath(file_path))
+app_loc = os.path.dirname(os.path.dirname(os.path.abspath(file_path)))
+print app_loc
 sys.path.append(app_loc)
 
-setting_loc = os.path.join(os.path.join(os.path.dirname(app_loc), "fridgefiller"), "settings.py")
 
-print setting_loc
-
-os.environ['DJANGO_SETTINGS_MODULE'] = setting_loc
-
-from django.conf import settings
-
-settings.configure()
-
-import django
+import os, django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fridgefiller.settings")
 django.setup()
 
+
 from django.contrib.auth.models import User
-from models import UserProfile, Party, ShoppingList, Pantry, Item, ItemDetail
+from lists.models import UserProfile, Party, ShoppingList, Pantry, Item, ItemDetail
 
 # pre: The database is empty
 # post: fills up the database with model entries
@@ -30,7 +24,7 @@ def generate_db():
     vork_u = User.objects.create(username="vork", first_name="Herman")
     bladezz_u = User.objects.create(username="bladezz", first_name="Simon")
     clara_u = User.objects.create(username="clara", first_name="clara")
-    
+
     users = [codex_u, zaboo_u, tink_u, vork_u, bladezz_u, clara_u]
 
     codex_up = UserProfile.objects.get(name="codex")
@@ -38,39 +32,42 @@ def generate_db():
     
     zaboo_up = UserProfile.objects.get(name="zaboo")
     zaboo_up.description = "annoying, warlock"
-
+    
     tink_up = UserProfile.objects.get(name="tinkerballa")
     tink_up.description = "entitled, ranger"
-
+    
     vork_up = UserProfile.objects.get(name="vork")
     vork_up.description = "cheap and pendantic, warrior"
-
-    bladezz_up = User.objects.create(name="bladezz")
+    
+    bladezz_up = UserProfile.objects.get(name="bladezz")
     bladezz_up.description = "immature, rogue"
-
-    clara_up = User.objects.create(name="clara")
+    
+    clara_up = UserProfile.objects.get(name="clara")
     clara_up.description = "mother, mage"
-
+    
     user_profiles = [codex_up, zaboo_up, tink_up, vork_up, bladezz_up, clara_up]
     for i in user_profiles:
         i.save()
-
-    p = Party.objects.create(name="Knights Of Good")
+    
+    p = Party.objects.create(name="Knights Of Good", owner=vork_up)
     p.owner = vork_up
     for i in [codex_up, zaboo_up, tink_up, bladezz_up, clara_up]:
         p.users.add(i)
 
-    shoplist = ShoppingList.objects.create(name="Raid Potions", owner=codex_up)
-
+    shoplist = ShoppingList.objects.create(name="Raid Potions")
+    
     shoplist.description = "potion list for molten core"
-
+    
     pots = [Item.objects.create(name="Elixir of Mighty Agility", description="Increases agility by 25 for 1 hour"),
             Item.objects.create(name="Elixir of Mighty Defenese", description="Increases armor by 180 for 1 hour"),
             Item.objects.create(name="Fel Strength", description="Increases attack power by 90 for 1 hour")]
-
+    
     for i in pots:
         i.save()
         shoplist.items.add(i)
+
+    for i in user_profiles:
+        shoplist.owners.add(i)
 
     shoplist.save()
 
@@ -90,7 +87,5 @@ def generate_db():
     guild_bank.save()
 
 
-
 if __name__ == "__main__":
-    print "Generating the database!"
     generate_db()
