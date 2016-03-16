5328f09ca97601d63c3549e2df48e906d147ab9f
brandon <bwp44f@mst.edu>
2016-03-16 16:31:39 -0500

    removed print statements

--- fridgefiller/lists/views.py
+++ fridgefiller/lists/views.py
@@ -189,14 +189,8 @@ class PrintListMiniView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(PrintListMiniView, self).get_context_data(**kwargs)
         user = UserProfile.objects.get(user=self.request.user)
-        
-        print(user)
-        print(user.lists.all())
 
         try:
-            print "printing lists"
-            for i in user.lists.all():
-                print i
             item_list = user.lists.get(pk=kwargs["list_id"])
         except :
             raise Http404
